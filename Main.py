c4b5ba9d8f13c9f13f89ea9958357c616fc67fe4
MadheshkumarM <110010925+MadheshkumarM@users.noreply.github.com>
2022-09-20 12:02:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,4 +20,5 @@ for item in re.split('], |].', input_data):
   item = item[1:]
   data = item.split(', ')
   if len(data) > 1:
-    insert(HashTable, int(data[0]), data[1])
+    insert(HashTable, int(data[0]), data[1]
+	   
